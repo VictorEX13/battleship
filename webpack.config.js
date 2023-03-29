1f353473a3160fce1ea2007b030425649f5e705a
Victor <victorshiira4869@gmail.com>
2023-03-28 22:20:55 -0300

    Set up npm, jest, webpack & babel

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "/src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ title: "Battleship" })],
+  mode: "production",
+  devtool: "inline-source-map",
+};
